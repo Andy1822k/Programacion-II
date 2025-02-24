c06eaf3a24cf12e645d599ccfd7105e6d1c0c509
Andy1822k <146199834+Andy1822k@users.noreply.github.com>
2025-02-24 10:16:18 -0400

    Update Linea.java

--- Practica-2/Java/Linea.java
+++ Practica-2/Java/Linea.java
@@ -1,3 +1,5 @@
+package practica2;
+
 import java.awt.*;
 
 public class Linea {
@@ -8,17 +10,24 @@ public class Linea {
         this.p2 = p2;
     }
 
-    public void dibujaLinea(Graphics g, int height) {
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(Color.RED);
+    public void dibujaLinea(Graphics g, int height, int width) {
+    Graphics2D g2 = (Graphics2D) g;
+    g2.setColor(Color.RED);
 
-        int x1 = (int) (p1.x * 40);
-        int y1 = (int) (p1.y * 40);
-        int x2 = (int) (p2.x * 40);
-        int y2 = (int) (p2.y * 40);
+    int escala = 40; // Factor de escala
 
-        g2.drawLine(x1, height - y1, x2, height - y2);
-    }
+    // Centro del panel
+    int cx = width / 2;
+    int cy = height / 2;
+
+    // Ajustar coordenadas relativas al centro con conversión segura a int
+    int x1 = cx + Math.round(p1.x * escala);
+    int y1 = cy - Math.round(p1.y * escala); // Invertimos Y para que coincida con el sistema cartesiano
+    int x2 = cx + Math.round(p2.x * escala);
+    int y2 = cy - Math.round(p2.y * escala);
+
+    g2.drawLine(x1, y1, x2, y2);
+}
 
     @Override
     public String toString() {
