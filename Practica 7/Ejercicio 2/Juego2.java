ce0f82a7c752c48ce3c5ceb85a9f100496c54664
Andy1822k <146199834+Andy1822k@users.noreply.github.com>
2025-04-06 18:48:12 -0400

    Create Juego2.java

---
+++ Practica 7/Ejercicio 2/Juego2.java
@@ -0,0 +1,17 @@
+public class Juego2 {
+    protected int numeroDeVidas;
+    protected int record;
+    public void reiniciaPartida() 
+    {
+        numeroDeVidas = 3;  
+    }
+    public void actualizaRecord() {
+        if (numeroDeVidas > record) {
+            record = numeroDeVidas;
+        }
+    }
+    public boolean quitaVida() {
+        numeroDeVidas--;
+        return numeroDeVidas > 0;
+    }
+}
