20947af5aaa234486a13dd0afe0208656de435cc
Andy1822k <146199834+Andy1822k@users.noreply.github.com>
2025-04-06 18:53:28 -0400

    Create AppJuego2.java

---
+++ Practica 7/Ejercicio 2/AppJuego2.java
@@ -0,0 +1,10 @@
+public class AppJuego2 {
+    public static void main(String[] args) {
+        JuegoAdivinaNumero juegoNormal = new JuegoAdivinaNumero(3);
+        JuegoAdivinaPar juegoPar = new JuegoAdivinaPar(3);
+        JuegoAdivinaImpar juegoImpar = new JuegoAdivinaImpar(3);
+        juegoNormal.juega();
+        juegoPar.juega();
+        juegoImpar.juega();
+    }
+}
